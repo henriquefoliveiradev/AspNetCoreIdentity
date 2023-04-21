904f98d18a88e2bb1766f1a261db25a340ce652d
henroliveira <henrique_fo@outlook.com>
2023-04-21 01:07:17 -0300

    Adicionando arquivos do projeto

---
+++ AspNetCoreIdentity/Program.cs
@@ -0,0 +1,78 @@
+using AspNetCoreIdentity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+// *** Configurando serviços no container ***
+
+// Politica de cookies (LGPD)
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+// Adicionando suporte ao contexto do Identity via EF
+builder.Services.AddDbContext<AspNetCoreIdentityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AspNetCoreIdentityContextConnection")));
+
+// Adicionando configuração padrão do Identity
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AspNetCoreIdentityContext>();
+
+// Adicionando Autorizações personalizadas por policies
+//builder.Services.AddAuthorization(options =>
+//{
+//    options.AddPolicy("PodeExcluir", policy => policy.RequireClaim("PodeExcluir"));
+
+//    options.AddPolicy("PodeLer", policy => policy.Requirements.Add(new PermissaoNecessaria("PodeLer")));
+//    options.AddPolicy("PodeEscrever", policy => policy.Requirements.Add(new PermissaoNecessaria("PodeEscrever")));
+//});
+
+// Adicionando MVC no pipeline
+builder.Services.AddControllersWithViews();
+
+// Adicionando suporte a componentes Razor (ex. Telas do Identity)
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// *** Configurando o resquest dos serviços no pipeline *** 
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/erro/500");
+    app.UseStatusCodePagesWithRedirects("/erro/{0}");
+    app.UseHsts();
+}
+
+// Redirecionamento para HTTPs
+app.UseHttpsRedirection();
+
+// Uso de arquivos estáticos (ex. CSS, JS)
+app.UseStaticFiles();
+
+// Adicionando suporte a rota
+app.UseRouting();
+
+// Autenticacao e autorização (Identity)
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Rota padrão
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+// Mapeando componentes Razor Pages (ex: Identity)
+app.MapRazorPages();
+
+app.Run();
